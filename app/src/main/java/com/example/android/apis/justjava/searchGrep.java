ddd516f9edca3783b4be9fd7b2cf806b135a3d61
NatsumeReiko <sprix.reiko.natsume@gmail.com>
2015-09-17 12:53:55 +0900

    Adding 'newfolder' directory from 'new branch'.

---
+++ app/src/main/java/com/example/android/apis/justjava/searchGrep.java
@@ -0,0 +1,7 @@
+package com.example.android.apis.justjava;
+
+/**
+ * Created by ms2 on 2015/05/08.
+ */
+public class searchGrep {
+}
---
+++ app/src/main/java/com/example/android/apis/newfolder/NewFolderClass.java
@@ -0,0 +1,7 @@
+package com.example.android.apis.newfolder;
+
+/**
+ * Created by ms2 on 2015/09/17.
+ */
+public class NewFolderClass {
+}
